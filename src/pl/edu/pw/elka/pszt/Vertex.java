4272ef0d86daf6660d129e0220f5784860c2fff6
Kajo0 <kajo100@gmail.com>
2012-12-08 16:31:45 +0100

    Some structures Some tests in main function Comments attached Hope it'll work with next things ;]

--- src/pl/edu/pw/elka/pszt/Vertex.java
+++ src/pl/edu/pw/elka/pszt/Vertex.java
@@ -3,7 +3,9 @@ package pl.edu.pw.elka.pszt;
 import com.vividsolutions.jts.geom.Coordinate;
 
 /**
- * Vertex representation
+ * Vertex representation with id identifying it
+ * 
+ * @author Kajo
  */
 public class Vertex {
 
@@ -13,7 +15,7 @@ public class Vertex {
 	/** Coordinates */
 	private Coordinate coordinate;
 	
-	/** ID */
+	/** Unique vertex ID */
 	private int id = i++;
 
 	/**
@@ -38,40 +40,80 @@ public class Vertex {
 		this.coordinate = coordinate;
 	}
 
+	/**
+	 * Set x coordinate
+	 * 
+	 * @param x X coordinate
+	 */
 	public void setX(final int x)
 	{
 		this.coordinate.x = x;
 	}
 
+	/**
+	 * Get x coordinate
+	 * 
+	 * @return X coordinate
+	 */
 	public int getX()
 	{
 		return (int) this.coordinate.x;
 	}
 
-	public void setY(final int x)
+	/**
+	 * Set y coordinate
+	 * 
+	 * @param y Y coordinate
+	 */
+	public void setY(final int y)
 	{
-		this.coordinate.x = x;
+		this.coordinate.y = y;
 	}
 
+	/**
+	 * Get y coordinate
+	 * 
+	 * @return Y coordinate
+	 */
 	public int getY()
 	{
-		return (int) this.coordinate.x;
+		return (int) this.coordinate.y;
 	}
 
+	/**
+	 * Get vertex id
+	 * 
+	 * @return Vertex ID
+	 */
 	public int getId()
 	{
 		return this.id;
 	}
 
-	public void setCoordinate(Coordinate coordinate) {
+	/**
+	 * Set coordinate by given coordinate
+	 * 
+	 * @param coordinate Coordinate to set
+	 */
+	public void setCoordinate(final Coordinate coordinate) {
 		this.coordinate = coordinate;
 	}
 
+	/**
+	 * Get coordinate representation of vertex
+	 * 
+	 * @return Vertex coordinate
+	 */
 	public Coordinate getCoordinate()
 	{
 		return this.coordinate;
 	}
 
+	/**
+	 * Get information about vertex
+	 * 
+	 * @return ID of vertex and coordinates
+	 */
 	public String toString()
 	{
 		return this.id + " : (" + (int) this.coordinate.x + ", " + (int) this.coordinate.y + ")";
