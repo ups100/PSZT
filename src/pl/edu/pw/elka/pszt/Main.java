7c7c92f294271bacc5901dcbd600dd64d7e17487
markubik <markubik@gmail.com>
2012-12-11 22:51:54 +0100

    Crossing and mutation, but is not working

--- src/pl/edu/pw/elka/pszt/Main.java
+++ src/pl/edu/pw/elka/pszt/Main.java
@@ -1,14 +1,10 @@
 package pl.edu.pw.elka.pszt;
-import java.awt.Toolkit;
-import java.util.LinkedList;
 
-import javax.swing.JFrame;
+
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
 
 /**
@@ -19,15 +15,17 @@ import com.vividsolutions.jts.geom.Polygon;
 public class Main {
 
 	/**
-	 * @param args No arguments
-	 * @throws Exception // Don't ask :P 
+	 * @param args
+	 *            No arguments
+	 * @throws Exception
+	 *             // Don't ask :P
 	 */
 	public static void main(String[] args) throws Exception // Don't ask :P
 	{
-		int[][] trianglePoints = { {0, 6}, {3, 9}, {3, 6} };
-		int[][] bigSquarePoints = { {0, 6}, {3, 6}, {3, 3}, {0, 3} };
-		int[][] smallSquarePoints = { {1, 3}, {3, 3}, {3, 1}, {1, 1} };
-		
+		int[][] trianglePoints = { { 0, 6 }, { 3, 9 }, { 3, 6 } };
+		int[][] bigSquarePoints = { { 0, 6 }, { 3, 6 }, { 3, 3 }, { 0, 3 } };
+		int[][] smallSquarePoints = { { 1, 3 }, { 3, 3 }, { 3, 1 }, { 1, 1 } };
+
 		Segment triangleSegment = new Segment(trianglePoints);
 		Segment bigSquareSegment = new Segment(bigSquarePoints);
 		Segment smallSquareSegment = new Segment(smallSquarePoints);
@@ -39,30 +37,29 @@ public class Main {
 
 		Population p = new Population(multiSegment);
 		System.out.println(p);
+		p.nextGeneration();
 		
-	//	p.copulateEntities();
+		// p.copulateEntities();
 	}
 
 	/**
 	 * Nothing special, just for test library linking
 	 */
-	public void testFunction()
-	{
-		Coordinate[] traingleCoords = new Coordinate[] {	new Coordinate(0,  0),
-															new Coordinate(0,  40),
-															new Coordinate(40, 40),
-															new Coordinate(0,   0) };
-		Coordinate[] squareCoords = new Coordinate[] {	new Coordinate(0,  0),
-														new Coordinate(0,  20),
-														new Coordinate(20, 20),
-														new Coordinate(20, 0),
-														new Coordinate(0,  0) };
+	public void testFunction() {
+		Coordinate[] traingleCoords = new Coordinate[] { new Coordinate(0, 0),
+				new Coordinate(0, 40), new Coordinate(40, 40),
+				new Coordinate(0, 0) };
+		Coordinate[] squareCoords = new Coordinate[] { new Coordinate(0, 0),
+				new Coordinate(0, 20), new Coordinate(20, 20),
+				new Coordinate(20, 0), new Coordinate(0, 0) };
 
 		GeometryFactory geometryFactory = new GeometryFactory();
 
-		LinearRing trianleRing = geometryFactory.createLinearRing(traingleCoords);
+		LinearRing trianleRing = geometryFactory
+				.createLinearRing(traingleCoords);
 		LinearRing squareRing = geometryFactory.createLinearRing(squareCoords);
-		LinearRing holes[] = null; // advise if any i think, 'use LinearRing[] to represent holes'
+		LinearRing holes[] = null; // advise if any i think, 'use LinearRing[]
+									// to represent holes'
 
 		Polygon triangle = geometryFactory.createPolygon(trianleRing, holes);
 		Polygon square = geometryFactory.createPolygon(squareRing, holes);
