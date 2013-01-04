e5c9429840a2432b528d01287a1dee1be6c54316
markubik <markubik@gmail.com>
2013-01-04 18:51:03 +0100

    nothing

--- src/pl/edu/pw/elka/pszt/MultiSegment.java
+++ src/pl/edu/pw/elka/pszt/MultiSegment.java
@@ -9,7 +9,8 @@ import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
 
 /**
- * Class containing representation of app model such as segments and target segment made of small tangram segments
+ * Class containing representation of app model such as segments and target
+ * segment made of small tangram segments
  * 
  * @author Marcin Kubik
  * @author Mikołaj Markiewicz
@@ -36,10 +37,10 @@ public class MultiSegment {
 	 * Adds segment to list of tangram segments building target multi polygon
 	 * Gather vertices information
 	 * 
-	 * @param segment Segment to add
+	 * @param segment
+	 *            Segment to add
 	 */
-	public void addSegment(final Segment segment)
-	{
+	public void addSegment(final Segment segment) {
 		this.segments.add(segment);
 		this.vertices.addAll(segment.getVertices()); // TODO without duplicates
 
@@ -49,10 +50,9 @@ public class MultiSegment {
 	/**
 	 * Create multi polygon from segments inside
 	 */
-	private void createPolygon()
-	{
+	private void createPolygon() {
 		Polygon[] polygons = new Polygon[this.segments.size()];
-		
+
 		for (int i = 0; i < this.segments.size(); ++i)
 			polygons[i] = this.segments.get(i).getPolygon();
 
@@ -62,19 +62,18 @@ public class MultiSegment {
 	}
 
 	/**
-	 * Calculates area of multipolygon (minus intersection things - only cover area)
+	 * Calculates area of multipolygon (minus intersection things - only cover
+	 * area)
 	 */
-	private void calculateArea()
-	{
+	private void calculateArea() {
 		double sum = 0;
 		double minus = 0;
-		for (int i = 0; i < this.segments.size(); ++i)
-		{
+		for (int i = 0; i < this.segments.size(); ++i) {
 			sum += this.segments.get(i).getPolygon().getArea();
 
-			for (int j = i + 1; j < this.segments.size(); ++j)
-			{
-				Geometry g = this.segments.get(i).getPolygon().intersection(this.segments.get(j).getPolygon());
+			for (int j = i + 1; j < this.segments.size(); ++j) {
+				Geometry g = this.segments.get(i).getPolygon()
+						.intersection(this.segments.get(j).getPolygon());
 
 				minus += g.getArea();
 			}
@@ -83,30 +82,44 @@ public class MultiSegment {
 		this.coveredArea = sum - minus;
 		this.overlapArea = minus;
 	}
-	
 
 	/**
 	 * Get random vertex from segment
 	 * 
 	 * @return Randomly chosen vertex
 	 */
-	public Vertex getRandomVertex()
-	{
+	public Vertex getRandomVertex() {
 		return this.vertices.get(new Random().nextInt(this.vertices.size()));
 	}
 
 	/**
-	 * Align vertices to top left corner by removing minimum of X and Y coordinate
+	 * Get vertex instance by given id from vertices list
+	 * 
+	 * @param id
+	 *            Vertex id
+	 * @return Vertex instance if found equal id
+	 * @throws Exception
+	 *             if no id found
+	 */
+	public Vertex getVertexById(final int id) {
+		for (Vertex v : this.vertices)
+			if (id == v.getId())
+				return v;
+
+		return null;
+	}
+
+	/**
+	 * Align vertices to top left corner by removing minimum of X and Y
+	 * coordinate
 	 * 
 	 * @return Aligned clone of segment
 	 */
-	public MultiSegment getAlignedMultiSegment()
-	{
+	public MultiSegment getAlignedMultiSegment() {
 		double minX = this.segments.get(0).getMinX();
 		double minY = this.segments.get(0).getMinY();
 
-		for (int i = 1; i < this.segments.size(); ++i)
-		{
+		for (int i = 1; i < this.segments.size(); ++i) {
 			double minXt = this.segments.get(i).getMinX();
 			if (minX > minXt)
 				minX = minXt;
@@ -126,12 +139,14 @@ public class MultiSegment {
 
 	/**
 	 * Calculate intersection area of this and given one
-	 * @param other Multi segment to check with
+	 * 
+	 * @param other
+	 *            Multi segment to check with
 	 * @return intersection area
-	 * @return array of sum of intersection source with target, source overlapping area, target covering area
+	 * @return array of sum of intersection source with target, source
+	 *         overlapping area, target covering area
 	 */
-	public double[] calculateIntersectionArea(final MultiSegment other)
-	{
+	public double[] calculateIntersectionArea(final MultiSegment other) {
 		MultiSegment target = this.getAlignedMultiSegment();
 		MultiSegment source = other.getAlignedMultiSegment();
 
@@ -140,14 +155,15 @@ public class MultiSegment {
 
 		double sumOfIntersection = 0;
 		for (int i = 0; i < sourceSegments.size(); ++i)
-			for (int j = 0; j < targetSegments.size(); ++j)
-			{
-				Geometry g = sourceSegments.get(i).getPolygon().intersection(targetSegments.get(j).getPolygon());
+			for (int j = 0; j < targetSegments.size(); ++j) {
+				Geometry g = sourceSegments.get(i).getPolygon()
+						.intersection(targetSegments.get(j).getPolygon());
 
 				sumOfIntersection += g.getArea();
 			}
 
-		return new double[] {sumOfIntersection, source.getOverlapArea(), target.getCoveredArea()};
+		return new double[] { sumOfIntersection, source.getOverlapArea(),
+				target.getCoveredArea() };
 	}
 
 	/**
@@ -155,8 +171,7 @@ public class MultiSegment {
 	 * 
 	 * @return List of segments building target tangram element
 	 */
-	public ArrayList<Segment> getSegments()
-	{
+	public ArrayList<Segment> getSegments() {
 		return this.segments;
 	}
 
@@ -165,8 +180,7 @@ public class MultiSegment {
 	 * 
 	 * @return List of vertices in all segments
 	 */
-	public ArrayList<Vertex> getVertices()
-	{
+	public ArrayList<Vertex> getVertices() {
 		return this.vertices;
 	}
 
@@ -175,8 +189,7 @@ public class MultiSegment {
 	 * 
 	 * @return Target polygon
 	 */
-	public MultiPolygon getPolygon()
-	{
+	public MultiPolygon getPolygon() {
 		return this.polygon;
 	}
 
@@ -185,87 +198,81 @@ public class MultiSegment {
 	 * 
 	 * @return Multi polygon covered area
 	 */
-	public double getCoveredArea()
-	{
+	public double getCoveredArea() {
 		return this.coveredArea;
 	}
 
 	/**
 	 * Get overlapping area
+	 * 
 	 * @return
 	 */
-	public double getOverlapArea()
-	{
+	public double getOverlapArea() {
 		return this.overlapArea;
 	}
 
 	/**
 	 * Getter for the Min X
+	 * 
 	 * @return Min X
 	 */
-	public double getMinX()
-	{
+	public double getMinX() {
 		double bestX = 99999;
-		for(Vertex v : this.vertices)
-		{
-			if(v.getX() < bestX)
+		for (Vertex v : this.vertices) {
+			if (v.getX() < bestX)
 				bestX = v.getX();
 		}
 		return bestX;
 	}
-	
+
 	/**
 	 * Getter for the Min Y
+	 * 
 	 * @return Min Y
 	 */
-	public double getMinY()
-	{
+	public double getMinY() {
 		double bestY = 9999;
-		for(Vertex v : this.vertices)
-		{
-			if(v.getY() < bestY)
+		for (Vertex v : this.vertices) {
+			if (v.getY() < bestY)
 				bestY = v.getY();
 		}
 		return bestY;
 	}
-	
+
 	/**
 	 * Getter for the Max X
+	 * 
 	 * @return Max X
 	 */
-	public double getMaxX()
-	{
+	public double getMaxX() {
 		double bestX = -9999;
-		for(Vertex v : this.vertices)
-		{
-			if(v.getX() > bestX)
+		for (Vertex v : this.vertices) {
+			if (v.getX() > bestX)
 				bestX = v.getX();
 		}
 		return bestX;
 	}
-	
+
 	/**
 	 * Getter for the Max Y
+	 * 
 	 * @return Max Y
 	 */
-	public double getMaxY()
-	{
+	public double getMaxY() {
 		double bestY = -9999;
-		for(Vertex v : this.vertices)
-		{
-			if(v.getY() > bestY)
+		for (Vertex v : this.vertices) {
+			if (v.getY() > bestY)
 				bestY = v.getY();
 		}
 		return bestY;
 	}
-	
+
 	/**
 	 * Get information about segments building target segment
 	 * 
 	 * @return Multi Polygon info
 	 */
-	public String toString()
-	{
+	public String toString() {
 		if (this.polygon != null)
 			return this.polygon.toString();
 		else
--- src/pl/edu/pw/elka/pszt/Statistics.java
+++ src/pl/edu/pw/elka/pszt/Statistics.java
@@ -26,184 +26,204 @@ import com.vividsolutions.jts.geom.Coordinate;
  */
 
 @SuppressWarnings("serial")
-public class Statistics extends JFrame{
+public class Statistics extends JFrame {
 
 	/** Vector of adaptations */
 	private final Vector<Double> adaptations;
-	
+
 	/** Population that is represented by the statistics */
 	private final Population p;
-	
+
 	/** For some measurements */
 	double time;
-	
+
 	/**
-	 * c-tor
-	 * Starts counting, how much time has passed
-	 * @param p First population
+	 * c-tor Starts counting, how much time has passed
+	 * 
+	 * @param p
+	 *            First population
 	 */
-	public Statistics(Population p)
-	{
+	public Statistics(Population p) {
 		super("Statistics");
 		this.setBackground(Color.WHITE);
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		adaptations = new Vector<Double>();
 		this.p = p;
-		this.time = (double)System.currentTimeMillis();
+		this.time = (double) System.currentTimeMillis();
 	}
-	
+
 	/**
 	 * Adds new adaptations to vector of adaptations
-	 * @param a adaptation (from 0 do 1)
+	 * 
+	 * @param a
+	 *            adaptation (from 0 do 1)
 	 */
-	public void addAdaptation(double a)
-	{
+	public void addAdaptation(double a) {
 		adaptations.add(a);
 	}
-	
+
 	/**
 	 * Used to show Frame with results, such as time, number of generations etc
-	 * @param p Last Generation
+	 * 
+	 * @param p
+	 *            Last Generation
 	 */
-	public void showResults(Population p) 
-	{
-		this.setTitle("Sequence reached in " + p.getGenerationNumber() +" generation ");
+	public void showResults(Population p) {
+		this.setTitle("Sequence reached in " + p.getGenerationNumber()
+				+ " generation ");
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		int scale = (int)dim.getWidth()/this.adaptations.size();
+		int scale = (int) dim.getWidth() / this.adaptations.size();
 		Vector<Coordinate> coordinates = new Vector<Coordinate>();
-		for(int i = 0; i<this.adaptations.size(); ++i)
-		{
-			coordinates.add(new Coordinate(scale*i,(dim.getHeight()/2)*this.adaptations.get(i)));
+		for (int i = 0; i < this.adaptations.size(); ++i) {
+			coordinates.add(new Coordinate(scale * i, (dim.getHeight() / 2)
+					* this.adaptations.get(i)));
 		}
-		this.time = (double)System.currentTimeMillis() - this.time;
-		this.time = (double)this.time/1000;
-		
+		this.time = (double) System.currentTimeMillis() - this.time;
+		this.time = (double) this.time / 1000;
+
 		paintGraph(coordinates, this.time);
-		this.setBounds(0,0,(int)dim.getWidth(),(int)dim.getHeight());
+		this.setBounds(0, 0, (int) dim.getWidth(), (int) dim.getHeight());
 		this.setVisible(true);
-		
+
 	}
-	
+
 	/**
 	 * Adds three panels with informations
-	 * @param adapt vector of coordinates used to paint
-	 * @param time how much time did it take to solve tangram
+	 * 
+	 * @param adapt
+	 *            vector of coordinates used to paint
+	 * @param time
+	 *            how much time did it take to solve tangram
 	 */
-	public void paintGraph(final Vector<Coordinate> adapt, double time)
-	{
-		this.add(new MyPanel(adapt), BorderLayout.CENTER);	
-		this.add(new UpInformation(time,p), BorderLayout.NORTH);
+	public void paintGraph(final Vector<Coordinate> adapt, double time) {
+		this.add(new MyPanel(adapt), BorderLayout.CENTER);
+		this.add(new UpInformation(time, p), BorderLayout.NORTH);
 		this.add(new DownInformation(this), BorderLayout.SOUTH);
 	}
-	
+
 	/**
 	 * Adds panel with graph
+	 * 
 	 * @author Marcin Kubik
 	 * @author Mikołaj Markiewicz
 	 * @author Krzysztof Opasiak
 	 */
-	private class MyPanel extends JPanel 
-	{
+	private class MyPanel extends JPanel {
 		Vector<Coordinate> adapt;
-		
+
 		/**
 		 * Creates panel with graph
-		 * @param adapt vector of coordinates
+		 * 
+		 * @param adapt
+		 *            vector of coordinates
 		 */
-		public MyPanel(final Vector<Coordinate> adapt)
-		{
+		public MyPanel(final Vector<Coordinate> adapt) {
 			this.setBackground(Color.WHITE);
 			this.adapt = adapt;
 			setVisible(true);
 		}
-		
-		public void paintComponent(Graphics g) 
-		{
+
+		public void paintComponent(Graphics g) {
 			this.setBackground(Color.WHITE);
 			Shape circle;
 			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 			Graphics2D g2 = (Graphics2D) g;
-			for(int i = 0; i<adapt.size()-1; ++i)
-			{
-				g2.drawLine((int)adapt.get(i).x, (int)(dim.getHeight() - adapt.get(i).y - 200),
-							(int)adapt.get(i+1).x, (int)(dim.getHeight() - adapt.get(i+1).y - 200));
-				circle = new Ellipse2D.Float((int) adapt.get(i).x -3, (int)(dim.getHeight() - adapt.get(i).y - 200) -3, 6.0f,6.0f);
+			for (int i = 0; i < adapt.size() - 1; ++i) {
+				g2.drawLine((int) adapt.get(i).x, (int) (dim.getHeight()
+						- adapt.get(i).y - 200), (int) adapt.get(i + 1).x,
+						(int) (dim.getHeight() - adapt.get(i + 1).y - 200));
+				circle = new Ellipse2D.Float((int) adapt.get(i).x - 3,
+						(int) (dim.getHeight() - adapt.get(i).y - 200) - 3,
+						6.0f, 6.0f);
 				g2.draw(circle);
 			}
-			circle = new Ellipse2D.Float((int) adapt.get(adapt.size()-1).x - 3, 
-										(int)(dim.getHeight() - adapt.get(adapt.size()-1).y -200) -3, 6.0f,6.0f);
+			circle = new Ellipse2D.Float(
+					(int) adapt.get(adapt.size() - 1).x - 3,
+					(int) (dim.getHeight() - adapt.get(adapt.size() - 1).y - 200) - 3,
+					6.0f, 6.0f);
 			g2.draw(circle);
-			float dash[] = {10.0f};
-			BasicStroke dashed = new BasicStroke(1.0f,
-                    BasicStroke.CAP_BUTT,
-                    BasicStroke.JOIN_MITER, 10.0f,dash,0.0f);
+			float dash[] = { 10.0f };
+			BasicStroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+					BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
 
 			g2.setStroke(dashed);
-			g2.drawLine(0, (int)(dim.getHeight() - adapt.get(adapt.size()-1).y -200), 
-							(int)dim.getWidth(), (int)(dim.getHeight() - adapt.get(adapt.size()-1).y - 200));
-		
-			g2.drawLine(0,(int)(dim.getHeight() - (dim.getHeight()/2) * 0.5 - 200), (int)dim.getWidth(), (int)(dim.getHeight() - (dim.getHeight()/2) * 0.5) - 200);
+			g2.drawLine(
+					0,
+					(int) (dim.getHeight() - adapt.get(adapt.size() - 1).y - 200),
+					(int) dim.getWidth(),
+					(int) (dim.getHeight() - adapt.get(adapt.size() - 1).y - 200));
+
+			g2.drawLine(
+					0,
+					(int) (dim.getHeight() - (dim.getHeight() / 2) * 0.5 - 200),
+					(int) dim.getWidth(),
+					(int) (dim.getHeight() - (dim.getHeight() / 2) * 0.5) - 200);
 		}
 	}
-	
+
 	/**
 	 * Adds panel with statistic informations
+	 * 
 	 * @author Marcin Kubik
 	 * @author Mikołaj Markiewicz
 	 * @author Krzysztof Opasiak
 	 */
-	private class UpInformation extends JPanel
-	{
-		
-		public UpInformation(double time, Population p)
-		{
+	private class UpInformation extends JPanel {
+
+		public UpInformation(double time, Population p) {
 			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-			
-			JLabel timeLabel = new JLabel("Time of calculations: "+ time + " seconds");
-			timeLabel.setPreferredSize(new Dimension((int)dim.getWidth(),(int)dim.getHeight()/20));
+
+			JLabel timeLabel = new JLabel("Time of calculations: " + time
+					+ " seconds");
+			timeLabel.setPreferredSize(new Dimension((int) dim.getWidth(),
+					(int) dim.getHeight() / 20));
 			this.add(timeLabel);
-			
-			JLabel generationsLabel = new JLabel("Number of generations: " + p.getGenerationNumber() + " generations");
-			generationsLabel.setPreferredSize(new Dimension((int)dim.getWidth(),(int)dim.getHeight()/20));
+
+			JLabel generationsLabel = new JLabel("Number of generations: "
+					+ p.getGenerationNumber() + " generations");
+			generationsLabel.setPreferredSize(new Dimension((int) dim
+					.getWidth(), (int) dim.getHeight() / 20));
 			this.add(generationsLabel);
-			
-			JLabel informationLabel = new JLabel("Lower dashed line is 50% adaptation, upper is 100% adaptation");
-			informationLabel.setPreferredSize(new Dimension((int)dim.getWidth(),(int)dim.getHeight()/20));
+
+			JLabel informationLabel = new JLabel(
+					"Lower dashed line is 50% adaptation, upper is expected adaptation");
+			informationLabel.setPreferredSize(new Dimension((int) dim
+					.getWidth(), (int) dim.getHeight() / 20));
 			this.add(informationLabel);
-			
-			this.setPreferredSize(new Dimension((int)dim.getWidth(), (int)dim.getHeight()/5));
-			
+
+			this.setPreferredSize(new Dimension((int) dim.getWidth(), (int) dim
+					.getHeight() / 5));
+
 		}
-		public void paintComponent(Graphics g)
-		{
-			
+
+		public void paintComponent(Graphics g) {
+
 		}
 	}
-	
+
 	/**
 	 * Adds panel with all adaptations
+	 * 
 	 * @author Marcin Kubik
 	 * @author Mikołaj Markiewicz
 	 * @author Krzysztof Opasiak
 	 */
-	private class DownInformation extends JPanel
-	{
-		public DownInformation(Statistics s)
-		{
+	private class DownInformation extends JPanel {
+		public DownInformation(Statistics s) {
 			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 			this.add(new JLabel("Sequence of adaptations is "));
-			for(Double d : s.adaptations)
-			{		
+			for (Double d : s.adaptations) {
 				DecimalFormat myFormatter = new DecimalFormat("#.###");
 				String output = myFormatter.format(d);
 				this.add(new JLabel(output));
 			}
-			this.setPreferredSize(new Dimension((int)dim.getWidth(), (int)dim.getHeight()/10));
+			this.setPreferredSize(new Dimension((int) dim.getWidth(), (int) dim
+					.getHeight() / 10));
 		}
-		public void paintComponent(Graphics g)
-		{
-			//Nothing to do here
+
+		public void paintComponent(Graphics g) {
+			// Nothing to do here
 		}
 	}
 }
